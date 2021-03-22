12c7035dd5d85218302e7e871fbe45ed5315f690
Miguel <miguelsr.1084@gmail.com>
2021-03-22 12:11:51 -0500

    commit inicial

---
+++ go.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"go_cero/greet"
+)
+
+func main() {
+	fmt.Println("comer")
+	greet.English()
+}
---
+++ greet/greet.go
@@ -0,0 +1,15 @@
+package greet
+
+// esta es una variable global
+
+var emoji = "ToT"
+
+// GreetEnglish
+func English() string{
+	return "Hi " + emoji
+}
+
+// GreetItalian
+func tItalian() string {
+	return "Ciao " + emoji
+}
